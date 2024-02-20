af6b5af096cd160a03a7eae35eda2d42a6bc2bfb
Sampo Savolainen <sampo.savolainen@gmail.com>
2024-02-20 21:12:58 +0200

    wip: add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mailItLogo from './assets/logo.png'
 import './App.css'
 
 function App() {
@@ -9,14 +8,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={mailItLogo} className="logo" alt="Mail-It logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="tagline">Never miss an important message</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
